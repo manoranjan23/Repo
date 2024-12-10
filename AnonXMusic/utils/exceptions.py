307ad30dc44150fd110777a9159f6677d93e3715
AOMusic <165138363+AOMusic@users.noreply.github.com>
2024-12-10 16:09:37 +0530

    Update exceptions.py

--- AnonXMusic/utils/exceptions.py
+++ AnonXMusic/utils/exceptions.py
@@ -5,20 +5,3 @@ class AssistantErr(Exception):
 
 class UnableToFetchCarbon(Exception):
     pass
-
-import subprocess
-
-class FFmpegNotInstalledException(Exception):
-    """Raised when FFmpeg is not installed"""
-    pass
-
-def check_ffmpeg():
-    try:
-        subprocess.run(['ffmpeg', '-version'], check=True)
-    except FileNotFoundError:
-        raise FFmpegNotInstalledException("FFmpeg is not installed")
-
-try:
-    check_ffmpeg()
-except FFmpegNotInstalledException as e:
-    print(e)
