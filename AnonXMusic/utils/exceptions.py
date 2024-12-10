b546a9050719708ff85a197fcbd4aef0ecee7b30
AOMusic <165138363+AOMusic@users.noreply.github.com>
2024-12-10 14:02:56 +0530

    Update exceptions.py

--- AnonXMusic/utils/exceptions.py
+++ AnonXMusic/utils/exceptions.py
@@ -6,10 +6,19 @@ class AssistantErr(Exception):
 class UnableToFetchCarbon(Exception):
     pass
 
+import subprocess
+
 class FFmpegNotInstalledException(Exception):
     """Raised when FFmpeg is not installed"""
     pass
 
-class PyTgCallsException(Exception):
-    """Raised when there is an error in PyTgCalls"""
-    pass
+def check_ffmpeg():
+    try:
+        subprocess.run(['ffmpeg', '-version'], check=True)
+    except FileNotFoundError:
+        raise FFmpegNotInstalledException("FFmpeg is not installed")
+
+try:
+    check_ffmpeg()
+except FFmpegNotInstalledException as e:
+    print(e)
