40c83ecfd4128f0ac64225b4a96c4f887eb176bd
AOMusic <165138363+AOMusic@users.noreply.github.com>
2024-12-10 13:51:11 +0530

    Update exceptions.py

--- AnonXMusic/utils/exceptions.py
+++ AnonXMusic/utils/exceptions.py
@@ -1,3 +1,27 @@
-class AssistantErr(Exception):
-    def __init__(self, errr: str):
-        super().__init__(errr)
+class InvalidFormatException(Exception):
+    """Raised when the format is invalid"""
+    pass
+
+class InvalidFileException(Exception):
+    """Raised when the file is invalid"""
+    pass
+
+class InvalidUrlException(Exception):
+    """Raised when the URL is invalid"""
+    pass
+
+class DownloadFailedException(Exception):
+    """Raised when the download fails"""
+    pass
+
+class ConversionFailedException(Exception):
+    """Raised when the conversion fails"""
+    pass
+
+class FFmpegNotInstalledException(Exception):
+    """Raised when FFmpeg is not installed"""
+    pass
+
+class PyTgCallsException(Exception):
+    """Raised when there is an error in PyTgCalls"""
+    pass
