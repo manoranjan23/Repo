f03780a7b095838cda61abad5c31a71d6a8ef2f2
AOMusic <165138363+AOMusic@users.noreply.github.com>
2024-12-13 15:19:21 +0530

    Update ads.py

--- ads.py
+++ ads.py
@@ -9,15 +9,20 @@ Pyrogram client
 app = Client("my_bot")
 
 
-Ad settings
 AD_TEXT = "Exclusive music updates!"
 GROUP_ID = "YOUR_GROUP_ID"
 
-Send ad function
 def send_ad(client, message):
     client.send_message(GROUP_ID, AD_TEXT)
 
-Command handler
+@app.on_message(filters.command('start'))
+def start_command(client, message):
+    message.reply_text("Welcome!")
+
+@app.on_message(filters.command('help'))
+def help_command(client, message):
+    message.reply_text("Help!")
+
 @app.on_message(filters.command('ad'))
 def send_ad_command(client, message):
     send_ad(client, message)
