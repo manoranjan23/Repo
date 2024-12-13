9a8b355bbaff89ca62b2755a67fd1660a24b3bc1
AOMusic <165138363+AOMusic@users.noreply.github.com>
2024-12-13 15:05:45 +0530

    Update ads.py

--- ads.py
+++ ads.py
@@ -1,14 +1,31 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-Ad settings
-AD_TEXT = "Get exclusive music updates!"
-AD_TITLE = "Music Updates"
-AD_BUTTON_TEXT = "Join Channel"
-AD_URL = "@music_updates"
+Telegram Ads API settings
+API_KEY = "YOUR_API_KEY"
+API_SECRET = "YOUR_API_SECRET"
 
-Ad keyboard
-AD_KEYBOARD = {"text": AD_TEXT, "buttons": [[{"text": AD_BUTTON_TEXT, "url": AD_URL}]]}
+Pyrogram client
+app = Client("my_bot")
 
+Ads function
 def send_ad(client, message):
-    message.reply_text(AD_TEXT)
+    # Ad settings
+    ad_text = "Get exclusive music updates!"
+    ad_title = "Music Updates"
+    ad_button_text = "Join Channel"
+    ad_url = "@music_updates"
+
+    # Ad keyboard
+    keyboard = InlineKeyboardMarkup([[InlineKeyboardButton(ad_button_text, url=ad_url)]])
+
+    # Send ad
+    message.reply_text(ad_text, reply_markup=keyboard)
+
+Command handler
+@app.on_message(filters.command('ad'))
+def send_ad_command(client, message):
+    send_ad(client, message)
+
+Bot start
+app.run()
